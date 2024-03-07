30f59467f9500f827ec83e951aa3bbbc06b11976
devhaute <devhaute@gmail.com>
2024-03-07 21:07:24 +0900

    chore: 초기 환경 세팅

---
+++ Spowift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spowift
+//
+//  Created by kai on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spowift/SpowiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpowiftApp.swift
+//  Spowift
+//
+//  Created by kai on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SpowiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
