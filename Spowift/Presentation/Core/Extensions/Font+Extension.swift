fe6afa274a5a5ad8f58a949c56a9fb4d806f6ffd
devhaute <devhaute@gmail.com>
2024-03-09 17:12:54 +0900

    style: Typography 부분 컨벤션 적용

--- Spowift/Presentation/Core/Extensions/Font+Extension.swift
+++ Spowift/Presentation/Core/Extensions/Font+Extension.swift
@@ -8,15 +8,10 @@
 import SwiftUI
 
 enum TypographyStyle {
-    case title1
-    case title2
-    case title3
-    case headline1
-    case headline2
-    case body1
-    case body2
-    case caption1
-    case caption2
+    case title1, title2, title3
+    case headline1, headline2
+    case body1, body2
+    case caption1, caption2
     
     var name: String {
         switch self {
@@ -31,31 +26,21 @@ enum TypographyStyle {
     
     var size: CGFloat {
         switch self {
-        case .title1:
-            return 24
-        case .title2:
-            return 20
-        case .title3:
-            return 18
-        case .headline1, .headline2:
-            return 16
-        case .body1, .body2:
-            return 14
-        case .caption1, .caption2:
-            return 12
+        case .title1: 24
+        case .title2: 20
+        case .title3: 18
+        case .headline1, .headline2: 16
+        case .body1, .body2: 14
+        case .caption1, .caption2: 12
         }
     }
     
     var lineHeight: CGFloat {
         switch self {
-        case .title1:
-            32
-        case .title2, .title3:
-            28
-        case .headline1, .headline2:
-            24
-        case .body1, .body2, .caption1, .caption2:
-            20
+        case .title1: 32
+        case .title2, .title3: 28
+        case .headline1, .headline2: 24
+        case .body1, .body2, .caption1, .caption2: 20
         }
     }
     
